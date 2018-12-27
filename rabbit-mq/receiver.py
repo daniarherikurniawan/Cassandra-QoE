bfe89bf4da6c9ee904ab4dee06cd33454504d97e
hyperpro <zhangbjb@126.com>
2018-12-27 04:46:39 -0600

    Update receiver.py

--- rabbit-mq/receiver.py
+++ rabbit-mq/receiver.py
@@ -3,27 +3,35 @@
 
 import pika
 import time
+import sys
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.0.2'))
+pika_credentials = pika.PlainCredentials('test1', 'test1')
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.0.2', credentials=pika_credentials))
 channel = connection.channel()
 
 channel.queue_declare(queue='TestQueue', durable=False)
 
 counter = 0
 start_time = 0.0
+last_time = 0.0
+now_time = 0.0
 
 def callback(ch, method, properties, body):
 
     global counter
     global start_time
+    global last_time
+    global now_time
     if counter == 0:
         start_time = time.time()
     counter += 1
+    last_time = now_time
     now_time = time.time()
     if now_time - start_time > 1.00:
-        print('Throughput: ', counter)
+        print('Throughput:', counter, 'Total Time Consumption:', now_time - start_time)
         counter = 0
-    print('[*] Received Message', counter)
+    print('[*] Received Message', counter, 'Time Consumption:', now_time-last_time, 'Message Length:', sys.getsizeof(body))
 
 
 channel.basic_consume(callback,
