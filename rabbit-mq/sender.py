60e3eac87302271692c400d120b7318cb67fbe00
hyperpro <zhangbjb@126.com>
2018-12-30 00:01:36 -0600

    Update sender.py

--- rabbit-mq/sender.py
+++ rabbit-mq/sender.py
@@ -12,12 +12,13 @@ message_num = MSGNUM
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
-channel.queue_declare(queue='TestQueue', durable=False)
+channel.queue_declare(queue='TestQueue', durable=True,exclusive=False, auto_delete=True)
+channel.confirm_delivery()
 
 string_load =  ''.join([random.choice(string.ascii_letters + string.digits) for nn in range(payload)])
 
 for x in range(0, message_num):
-    channel.basic_publish(exchange='', routing_key='TestQueue', body=string_load)
+    channel.basic_publish(exchange='', routing_key='TestQueue', body=string_load,  properties=pika.BasicProperties(delivery_mode=2))
     print('[*] Send Successfully! Message', str(x))
 
 connection.close()
