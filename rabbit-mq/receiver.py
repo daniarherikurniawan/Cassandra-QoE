47622ad593506282c24805ded38b81542a409c6b
hyperpro <zhangbjb@126.com>
2018-12-31 12:14:35 -0600

    Update receiver.py

--- rabbit-mq/receiver.py
+++ rabbit-mq/receiver.py
@@ -10,7 +10,14 @@ pika_credentials = pika.PlainCredentials('test1', 'test1')
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.0.2', credentials=pika_credentials))
 channel = connection.channel()
 
-channel.queue_declare(queue='TestQueue', durable=True, exclusive=False, auto_delete=True)
+
+max_priority = 250
+properties = dict()
+properties['x-max-priority'] = max_priority
+
+channel = connection.channel()
+
+channel.queue_declare(queue='TestQueue', durable=True, exclusive=False, auto_delete=True,arguments=properties)
 
 counter = 0
 start_time = 0.0
@@ -32,7 +39,9 @@ def callback(ch, method, properties, body):
         print('Throughput:', counter, 'Total Time Consumption:', now_time - start_time)
         counter = 0
     ch.basic_ack(delivery_tag = method.delivery_tag)
-    print('[*] Received Message', counter, 'Time Consumption:', now_time-last_time, 'Message Length:', sys.getsizeof(body))
+    msgarray = str(body).split()
+    print('[*] Received Message', msgarray[0])
+    #print('[*] Received Message', counter, 'Time Consumption:', now_time-last_time, 'Message Length:', sys.getsizeof(body))
 
 channel.basic_qos(prefetch_count = 1)
 channel.basic_consume(callback,
