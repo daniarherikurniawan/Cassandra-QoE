c6094f9fa2dac4bbbb336964c96fe585223faa4c
hyperpro <zhangbjb@126.com>
2019-01-01 00:11:58 -0600

    Update receiver_tm.py

--- rabbit-mq/receiver_tm.py
+++ rabbit-mq/receiver_tm.py
@@ -24,6 +24,7 @@ def callback(ch, method, properties, body):
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
+channel.basic_qos(prefetch_count = 1)
 channel.basic_consume(callback,
                       queue='TestQueue',
                       no_ack=False)
