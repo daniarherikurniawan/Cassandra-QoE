e5f5c7d94c0aa4304672ac1d939523a6b84178fe
hyperpro <zhangbjb@126.com>
2018-12-31 22:32:25 -0600

    Create receiver_tm.py

---
+++ rabbit-mq/receiver_tm.py
@@ -0,0 +1,32 @@
+# Run this on client side (request USING rabbit-mq)
+#!/usr/bin/env python
+
+import pika
+import time
+import sys
+
+pika_credentials = pika.PlainCredentials('test1', 'test1')
+
+max_priority_num = 250
+c_properties  = dict()
+c_properties['x-max-priority'] = max_priority_num
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.0.2', credentials=pika_credentials))
+channel = connection.channel()
+
+channel.queue_declare(queue='TestQueue', durable=True, exclusive=False, auto_delete=True, arguments = c_properties)
+
+
+def callback(ch, method, properties, body):
+
+    msgarray = str(body).split()
+    print('[*] Received Message', msgarray[0])
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_consume(callback,
+                      queue='TestQueue',
+                      no_ack=False)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
