fa566612ae2c6933faf2145e92cf20a1358e2499
hyperpro <zhangbjb@126.com>
2019-01-01 23:29:47 -0600

    Update

--- rabbit-mq/rabbitmq_sender.sh
+++ rabbit-mq/rabbitmq_sender.sh
@@ -4,7 +4,7 @@ for prio in '10' '9' '8' '7' '6' '5' '4' '3' '2' '1'
 do
     for wkl in '10' '50' '60' '90' '120' '150' '180' '210' '220' '225' '230' '235' '240' '245' '250' '255' '260'
     do
-        echo 'Start priority $prio workload $wkl'
+        echo 'Start priority '$prio' workload '$wkl
         python 50000 15000 $wkl $prio >> senderfile_temp.txt
         echo 'Send Complete!'
         sleep 1m
