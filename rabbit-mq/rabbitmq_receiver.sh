6e4e55a56ecba3a1250503e3b6c7ea9acea5fbaf
hyperpro <zhangbjb@126.com>
2019-01-01 23:17:12 -0600

    Update

--- rabbit-mq/rabbitmq_receiver.sh
+++ rabbit-mq/rabbitmq_receiver.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-echo 'start successful for
-echo 'start successful +2'
 
+for prio in '10' '9' '8' '7' '6' '5' '4' '3' '2' '1'
+do
+    for wkl in '10' '50' '60' '90' '120' '150' '180' '210' '220' '225' '230' '235' '240' '245' '250' '255' '260'
+    do
+        echo 'Start priority $prio workload $wkl'
+        python3 receiver_tm.py > 'prio_'$prio'_workload_'$wkl.txt
+        sleep 2s
+    done
+done
