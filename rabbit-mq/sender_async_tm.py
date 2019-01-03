61613a86122867c5c40f9a144d80ce837c397268
hyperpro <zhangbjb@126.com>
2019-01-02 23:16:58 -0600

    Update

--- rabbit-mq/sender_async_tm.py
+++ rabbit-mq/sender_async_tm.py
@@ -258,7 +258,7 @@ class RabbitMQTest(object):
         if self._message_number < self._message_totalnum:
             self.PUBLISH_INTERVAL = random.expovariate(self._dislamba)
             # print('Published Interval Setting:', round(self.PUBLISH_INTERVAL * 1000), 'ms')
-            self.PUBLISH_INTERVAL = max(0, self.PUBLISH_INTERVAL - 0.001)
+            # self.PUBLISH_INTERVAL = max(0, self.PUBLISH_INTERVAL - 0.001)
             self.schedule_next_message()
         else:
             print('Mission Complete! Program Exit.')
