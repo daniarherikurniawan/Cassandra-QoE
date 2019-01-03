e48ab2989bd3ef401a80d7538e48dc928c521658
hyperpro <zhangbjb@126.com>
2019-01-03 03:43:43 -0600

    Update

--- rabbit-mq/rabbitmq_sender.sh
+++ rabbit-mq/rabbitmq_sender.sh
@@ -3,7 +3,7 @@
 for wkl in '30' '35' '40' '45' '50' '55' '60' '65' '70'
 do
     echo 'Start workload '$wkl'rps'
-    python sender_async_tm.py 200000 1500 $wkl >> senderfile_temp.txt
+    python sender_async_tm.py 200000 3500 $wkl >> senderfile_temp.txt
     echo 'Send Complete!'
     sleep 1m
 done
