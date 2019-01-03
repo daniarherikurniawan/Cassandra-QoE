96df7a78dd405bdb0172f8e06fd40525f6d1e473
hyperpro <zhangbjb@126.com>
2019-01-03 04:36:21 -0600

    Update

--- rabbit-mq/rabbitmq_receiver.sh
+++ rabbit-mq/rabbitmq_receiver.sh
@@ -2,7 +2,7 @@
 
 mkdir results
 
-for wkl in '80' '90' '100' '105' '110' '115' '120' '125' '130'
+for wkl in '80' '90' '100' '105' '110' '115' '120' '125' '126' '127' '128' '129' '130'
 do
     echo 'Start listen workload '$wkl'rps'
     python3 receiver_tm.py > 'results/workload_'$wkl.txt
--- rabbit-mq/rabbitmq_sender.sh
+++ rabbit-mq/rabbitmq_sender.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for wkl in '80' '90' '100' '105' '110' '115' '120' '125' '130'
+for wkl in '80' '90' '100' '105' '110' '115' '120' '125' '126' '127' '128' '129' '130'
 do
     echo 'Start workload '$wkl'rps'
     python sender_async_tm.py 100000 4500 $wkl >> senderfile_temp.txt
