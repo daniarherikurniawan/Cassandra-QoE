8df4775bdf9c3728eff9c5363e573a39bb09e44a
hyperpro <zhangbjb@126.com>
2019-01-14 03:12:45 -0600

    Update

--- rabbit-mq/receiver.py
+++ rabbit-mq/receiver.py
@@ -49,7 +49,7 @@ def callback(ch, method, properties, body):
         print('Throughput:', counter, 'Total Time Consumption:', now_time - start_time)
         counter = 0
     ch.basic_ack(delivery_tag = method.delivery_tag)
-    time.sleep(0.00083)
+    time.sleep(0.0082)
     #msgarray = str(body).split()
     #print('[*] Received Message', msgarray[0])
     #print('[*] Received Message', counter, 'Time Consumption:', now_time-last_time, 'Message Length:', sys.getsizeof(body))
