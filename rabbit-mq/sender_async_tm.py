6294df5c91dd9e299118da7975531fa759feee41
hyperpro <zhangbjb@126.com>
2019-01-14 00:33:24 -0600

    Update

--- rabbit-mq/sender_async_tm.py
+++ rabbit-mq/sender_async_tm.py
@@ -266,7 +266,7 @@ class RabbitMQTest(object):
         '''
         # message format: current_time + ' ' + priority + ' ' + a long string
         #t_time = time.time()
-        message = str(int(round(time.time() * 1000))) + ' ' + str(r_priority) + ' ' + self._string_load
+        message = str(int(round(time.time() * 1000))) + ' ' + str(r_priority) + ' '
         #print('time consumption', (time.time() - t_time)*1000)
 
 
