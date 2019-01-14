2a8b6b9b7034bb37bde87573d2978d69425df0cc
hyperpro <zhangbjb@126.com>
2019-01-14 02:42:13 -0600

    Update

--- rabbit-mq/receiver.py
+++ rabbit-mq/receiver.py
@@ -7,7 +7,7 @@ import sys
 
 pika_credentials = pika.PlainCredentials('test1', 'test1')
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.1.2', credentials=pika_credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='155.98.39.140', credentials=pika_credentials))
 channel = connection.channel()
 
 
@@ -15,6 +15,7 @@ max_priority_num = 250
 c_properties = dict()
 c_properties['x-max-priority'] = max_priority_num
 c_properties['x-message-ttl'] = 10000000
+c_properties['x-max-length'] = 10000000
 
 
 channel.exchange_declare(exchange='logs',
@@ -47,7 +48,7 @@ def callback(ch, method, properties, body):
     if now_time - start_time > 1.00:
         print('Throughput:', counter, 'Total Time Consumption:', now_time - start_time)
         counter = 0
-    #ch.basic_ack(delivery_tag = method.delivery_tag)
+    ch.basic_ack(delivery_tag = method.delivery_tag)
     #msgarray = str(body).split()
     #print('[*] Received Message', msgarray[0])
     #print('[*] Received Message', counter, 'Time Consumption:', now_time-last_time, 'Message Length:', sys.getsizeof(body))
@@ -55,7 +56,7 @@ def callback(ch, method, properties, body):
 channel.basic_qos(prefetch_count = 1)
 channel.basic_consume(callback,
                       queue='TestQueue',
-                      no_ack=True)
+                      no_ack=False)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
