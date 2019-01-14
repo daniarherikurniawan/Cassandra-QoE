7c6188d01b7b7a59a7c400b16ff08a16f8c8501b
hyperpro <zhangbjb@126.com>
2019-01-14 00:47:56 -0600

    Update

--- rabbit-mq/sender_async_tm.py
+++ rabbit-mq/sender_async_tm.py
@@ -231,8 +231,8 @@ class RabbitMQTest(object):
         message to be delivered in PUBLISH_INTERVAL seconds.
 
         """
-        self.publish_message()
-        #self._connection.ioloop.add_timeout(self.PUBLISH_INTERVAL, self.publish_message)
+        # self.publish_message()
+        self._connection.ioloop.add_timeout(self.PUBLISH_INTERVAL, self.publish_message)
 
     def publish_message(self):
         """If the class is not stopping, publish a message to RabbitMQ,
