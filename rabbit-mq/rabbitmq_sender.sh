b3e4610f3c83debe75ed78181f73aff993310d8b
hyperpro <zhangbjb@126.com>
2019-01-14 02:46:18 -0600

    Update

--- rabbit-mq/rabbitmq_sender.sh
+++ rabbit-mq/rabbitmq_sender.sh
@@ -6,7 +6,7 @@ do
     echo 'Start PRIORITY workload '$wkl'rps'
     python3 sender_async_tm.py 100000 0 $wkl >> senderfile_temp.txt
     echo 'Workload Complete!'
-    sleep 1m
+    sleep 30s
 done
 
 for wkl in '90' '95' '100' '105' '110' '115' '120'
@@ -14,7 +14,7 @@ do
     echo 'Start FIFO workload '$wkl'rps'
     python3 sender_async_tm.py 100000 1 $wkl >> senderfile_temp.txt
     echo 'Workload Complete!'
-    sleep 1m
+    sleep 30s
 done
 
 rm senderfile_temp.txt
