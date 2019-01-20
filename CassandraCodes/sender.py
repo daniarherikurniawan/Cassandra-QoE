f5cc214199b1cce143fd51218a88a466dc870aba
hyperpro <zhangbjb@126.com>
2019-01-19 20:36:31 -0600

    Update

--- CassandraCodes/sender.py
+++ CassandraCodes/sender.py
@@ -13,9 +13,9 @@ class Sender:
         self.session = self.generate_session()
         self.session.set_keyspace('ycsb')
         self.session.execute('use ycsb')
-        self.read_prepare_stmt = self.session.prepare('select * from usertable where token(y_id)=token(?)')
+        self.read_prepare_stmt = self.session.prepare('select * from usertable where y_id=?')
         self.update_prepare_stmt = self.session.prepare(
-            'update usertable set field0=?, field1=?, field2=?, field3=?, field4=?, field5=?, field6=?, field7=?, field8=?, field9=? where token(y_id)=token(?) if exists')
+            'update usertable set field0=?, field1=?, field2=?, field3=?, field4=?, field5=?, field6=?, field7=?, field8=?, field9=? where y_id=? if exists')
         self.scan_prepare_stmt = self.session.prepare('select * from usertable where token(y_id)>token(?) limit 100')
 
 
