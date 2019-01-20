e925942ceb91812607251db2b8ffec730a5977fb
hyperpro <zhangbjb@126.com>
2019-01-19 18:54:27 -0600

    Update

--- CassandraCodes/workload_generator.py
+++ CassandraCodes/workload_generator.py
@@ -6,9 +6,12 @@ import time
 import uuid
 import string
 
-system_size = 4000000000
+system_size = 2500000000
 
 def sys_main(row_size):
+
+    random.seed(time.time())
+
     row_num = int(system_size / row_size)
     col_name = ['y_id']
     field_size = int(row_size / 10)
@@ -18,12 +21,20 @@ def sys_main(row_size):
         field_name = 'field' + str(i)
         col_name.append(field_name)
 
+    payloads = []
+    payloads_size = 100
+    for i in range(0, payloads_size):
+        payload = ''.join([random.choice(string.ascii_letters + string.digits) for nn in range(field_size)])
+        payloads.append(payload)
+
     # creat dataframe
     df = pd.DataFrame(columns=col_name)
     for i in range(0, row_num):
+        if i % 1000 == 0:
+            print(i, row_num, int(i/row_num*100), '%')
         new_col = [uuid.uuid1()]
         for j in range(1, 11):
-            payload = ''.join([random.choice(string.ascii_letters + string.digits) for nn in range(field_size)])
+            payload = random.sample(payloads, 1)[0]
             new_col.append(payload)
         df_t = pd.DataFrame([new_col], columns=col_name)
         df = pd.concat([df, df_t], ignore_index=True)
