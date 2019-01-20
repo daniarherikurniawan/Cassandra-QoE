5876cdcf6c71329a7efea5fa6b7f63f419113411
hyperpro <zhangbjb@126.com>
2019-01-19 22:55:41 -0600

    Update

--- CassandraCodes/cassandra_trace.sh
+++ CassandraCodes/cassandra_trace.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir results
-for ((t=40; t<=60; t++))
+for ((t=40; t<=65; t++))
 do
     python3 cassandraclient.py $t
     mv cass_read_latency.txt 'results/cass_read_latency_'$t.txt
