8f1c9b7efa18383ccc82aa51d9524fa52f776c32
hyperpro <zhangbjb@126.com>
2019-01-20 04:00:35 -0600

    Update

--- CassandraCodes/cassandraclient.py
+++ CassandraCodes/cassandraclient.py
@@ -70,6 +70,7 @@ def sys_main(target_throughput):
         else:
             random.shuffle(payloads)
             req_sender.get_update_latency_non_block(host=hosts[0], user_id=user_id, fields=payloads)
+        print('sleeping time',time_intervals[times]/1000)
         time.sleep(time_intervals[times]/1000)
 
     time.sleep(100)
